e91331d1fc24373bdf78121713c0d441032b4b7e
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-05-04 16:44:29 -0500

    Create detectPangram.js

---
+++ detectPangram.js
@@ -0,0 +1,58 @@
+/* A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation. */
+
+function isPangram(string){
+  // create an array of every lowercase letter
+ const alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  
+  // create an array from string lowercase letters
+  let arr = string.toLowerCase().split("");
+  
+  // filter out punctuation and whitespace
+  arr = arr.filter(char => /[a-z]/g.test(char));
+  
+  // remove all repeating letters in arr
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        arr.splice(j, 1);
+      }
+    }
+  }
+  
+  // compare length of arr to length of alpha
+  if (arr.length === alpha.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// =========================================== //
+
+
+function isPangram(string){
+  return 'abcdefghijklmnopqrstuvwxyz'
+    .split('')
+    .every((x) => string.toLowerCase().includes(x));
+}
+
+function isPangram(string){
+  return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
+}
+
+function isPangram(str) {
+    var s = str.toLowerCase();
+    var letters = "zqxjkvbpygfwmucldrhsnioate";
+    for (var i = 0; i < 26; i++)
+        if (s.indexOf(letters.charAt(i)) == -1)
+            return false;
+    return true;
+}
+
+function isPangram(string) {
+  const alphabetList = [...'abcdefghijklmnopqrstuvwxyz'];
+
+  return alphabetList.every((letter) => string.toLowerCase().includes(letter));
+}
